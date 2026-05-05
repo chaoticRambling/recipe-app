05d1da405ba8888520926342c2d77d6a2c962263
“Alex <ablumenthal6@gatech.edu>
2026-05-04 22:11:02 -0400

    refactor: replace table with responsive card-based layout in RecipeList component

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -10,32 +10,36 @@ export default function RecipeList({ recipes }) {
 
   return (
     <div className="recipe-list-container">
-      <table className="recipe-list-table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Cuisine</th>
-            <th className="right-align">Prep Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          {recipes.map((recipe) => (
-            <tr 
-              key={recipe.id} 
-              className="recipe-row"
-              onClick={() => navigate('/recipe/' + recipe.id)}
-            >
-              <td className="recipe-title">{recipe.title}</td>
-              <td>
-                <span className="recipe-cuisine">{recipe.cuisine_type}</span>
-              </td>
-              <td className="right-align recipe-time">
-                {recipe.prep_time_minutes} mins
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className="recipe-list-header">
+        <div>Title</div>
+        <div>Cuisine</div>
+        <div style={{ textAlign: 'right' }}>Prep Time</div>
+        <div></div>
+      </div>
+      
+      {recipes.map((recipe) => (
+        <div 
+          key={recipe.id} 
+          className="recipe-card"
+          onClick={() => navigate('/recipe/' + recipe.id)}
+        >
+          <div className="cell-title">{recipe.title || 'Untitled Recipe'}</div>
+          
+          <div className="cell-cuisine">
+            {recipe.cuisine_type && (
+              <span className="pill-cuisine">{recipe.cuisine_type}</span>
+            )}
+          </div>
+          
+          <div className="cell-time">
+            <span className="pill-time">{recipe.prep_time_minutes} mins</span>
+          </div>
+          
+          <div className="cell-action">
+            <span className="chevron">&rsaquo;</span>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
